cad5664e2ecadf475f9da4a777dc8e2b8ef8a180
zainosaurus <zainosaurus@users.noreply.github.com>
2015-01-31 17:31:44 -0500

    system l&f

--- Files/MainRunner.java
+++ Files/MainRunner.java
@@ -4,6 +4,14 @@
 
 public class MainRunner {
     public static void main(String[] args) {
+    	// Don't touch
+    	try {
+            // Set System L&F
+        UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+    	}
+    	catch (Exception e) {}
+
         MainWindow wind = new MainWindow("MusicPlayer-3.14", new MusicManager());
     }
 }
